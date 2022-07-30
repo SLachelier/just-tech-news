909d3f22b7a5580c8fa94733b2fc156c53a4ad18
Shauna L <slachelier@gmail.com>
2022-07-29 22:33:26 -0500

    added password hashing

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//turns on routes
+// turn on routes
 app.use(routes);
 
-//turns on connection to db and server
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
